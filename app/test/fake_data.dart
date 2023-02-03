52846cb9d8e455171782af847e2752ef5b0c3dcc
minhnt3 <minhnt3@nal.vn>
2023-02-03 15:50:06 +0700

    login bloc test

---
+++ app/test/fake_data.dart
@@ -0,0 +1,5 @@
+import 'package:shared/shared.dart';
+
+const inputEmail = 'email';
+const inputPassword = 'password';
+const validationError = ValidationException(ValidationExceptionKind.invalidEmail);
